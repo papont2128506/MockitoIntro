a1b64e41e2aac12e9fea53770e0c5326f4a8bbd2
Andrey Trusov <trusov@haulmont.com>
2022-05-10 02:28:27 +0400

    lesson 3

--- src/test/java/ru/papont/DogTest.java
+++ src/test/java/ru/papont/DogTest.java
@@ -1,14 +1,21 @@
 package ru.papont;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DogTest {
 
+    private Dog dog;
+
+    @BeforeEach
+    void prepareData() {
+        dog = new Dog("Albert", 2);
+    }
+
     @Test
     void testNameDogIsNotBlank() {
-        Dog dog = new Dog("Albert", 2);
         assertEquals("Albert", dog.getName());
         dog.setName("Roman");
         assertEquals("Albert", dog.getName());
@@ -16,9 +23,9 @@ class DogTest {
 
     @Test
     void testNameDogIsBlank() {
-        Dog dog = new Dog("", 2);
-        dog.setName("Albert");
-        assertEquals("Albert", dog.getName());
+        dog = new Dog("", 2);
+        dog.setName("Roman");
+        assertEquals("Roman", dog.getName());
     }
 
     @Test
